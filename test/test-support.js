f54a29ef44f4ed5ea86fbca8f13ba863d8820bb3
Sajera <allsajera@gmail.com>
2016-12-21 12:54:31 +0200

    provide tests

--- test/test-support.js
+++ test/test-support.js
@@ -7,30 +7,10 @@ var is = require('../is.min.js');
 // require('chai').should();
 var expect = require('chai').expect;
 var assert = require('chai').assert;
-/**
- * use dualized a function dual times
- *
- * @param method: { String }
- * @param data: { Any }
- * @returns: { Boolean }
- */
-function dual ( method, data, data2 ) {
-
-    // return is[method](data, data2)&&is(method, data, data2);
-    console.log(arguments, method, is[method])
-    is['platform']('node')
-    // return is[method](data, data2)&&is(method, data, data2);
-}
 
 describe('SUPPORT', function() {
 
     it('platform Node', function () {
-
-// is.platform.node()
-// is.platform('node')
-// is('platform','node')
-// is['platform']('node')
-// dual('null', 'browser');
         expect( is('platform','node')&&is['platform']('node')&&is.platform.node() ).to.equal(true);
         expect( is('platform','browser')&&is['platform']('browser')&&is.platform.browser() ).to.equal(false);
     });
